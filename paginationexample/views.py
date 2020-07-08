9ef7f47ed6de4b6f7e294bacb7bedd886022e820
Dipendra <dipu.danish@outlook.com>
2020-07-08 21:23:35 +0545

    pagination

---
+++ paginationexample/views.py
@@ -0,0 +1,14 @@
+from blog.models import Post
+from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage,PageNotAnInteger
+def post_list(request):
+    object_list = Post.objects.all()
+    paginator = Paginator(object_list, 3) # 3 posts in each page
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    return render(request,'bloglist.html',{'page': page,'posts': posts})
